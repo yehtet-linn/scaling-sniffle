052624a76d81615883347c17ea348683266a9282
Ye Htet Lin <yehtet.linyn@gmail.com>
2024-02-28 23:14:04 +0630

    use createContext hook

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { ReactNode, useState, useReducer, ChangeEvent } from "react";
+import { useCounter, useCounterText } from "../context/CounterContext";
 
 type counterProps = {
   setCount: React.Dispatch<React.SetStateAction<number>>;
@@ -10,66 +11,32 @@ type ChildrenType = {
   children: (num: number) => ReactNode;
 };
 
-const initState = { count: 0, text: "" };
-
-enum ReducerType {
-  Increment,
-  Decrement,
-  NewInput,
-}
-
-type ReducerActionType = {
-  type: ReducerType;
-  payload?: string;
-};
-
-const reducer = (
-  state: typeof initState,
-  action: ReducerActionType
-): typeof initState => {
-  switch (action.type) {
-    case ReducerType.Increment:
-      return { ...state, count: state.count + 1 };
-    case ReducerType.Decrement:
-      return { ...state, count: state.count - 1 };
-    case ReducerType.NewInput:
-      return { ...state, text: action.payload ?? "" };
-    default:
-      throw new Error("Invalid Reducer Type");
-  }
-};
-
 // const Counter = ({ setCount, children }: counterProps) => {
 
 const Counter = ({ children }: ChildrenType) => {
   // const [count, setCount] = useState<number>(1);
 
-  // const increasement = () => setCount((prev) => prev + 1);
-  // const decreasement = () => setCount((prev) => prev - 1);
+  // const increment = () => setCount((prev) => prev + 1);
+  // const decrement = () => setCount((prev) => prev - 1);
 
-  const [state, dispatch] = useReducer(reducer, initState);
-
-  const increasement = () => dispatch({ type: ReducerType.Increment });
-  const decreasement = () => dispatch({ type: ReducerType.Decrement });
-  const handleTextINput = (e: ChangeEvent<HTMLInputElement>) => {
-    dispatch({ type: ReducerType.NewInput, payload: e.target.value });
-  };
+  const { count, increment, decrement } = useCounter();
+  const { text, handleTextInput } = useCounterText();
 
   return (
     <>
       {/* <div>Counter is {count}</div> */}
       {/* <div>{children(count)}</div> */}
-      <div>{children(state.count)}</div>
+      <div>{children(count)}</div>
       <div>
-        <button type="button" onClick={increasement}>
+        <button type="button" onClick={increment}>
           +
         </button>
-        <button type="button" onClick={decreasement}>
+        <button type="button" onClick={decrement}>
           -
         </button>
       </div>
-      <input type="text" onChange={handleTextINput} title="new input" />
-      <h2>{state.text}</h2>
+      <input type="text" onChange={handleTextInput} title="new input" />
+      <h2>{text}</h2>
     </>
   );
 };
